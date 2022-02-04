db6b6087159c19b0eb7f44f1f5687a2529feb7cf
Yumi <yuma.gugerli@gmail.com>
2022-02-05 00:10:30 +0100

    :construction: Get recipecards from database

--- src/screens/RecipeOverview.tsx
+++ src/screens/RecipeOverview.tsx
@@ -1,16 +1,21 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { StyleSheet, View, Text, FlatList } from 'react-native';
 import { RootStackParamList } from '../navigation/PeepoNavigation';
-import Tag  from '../components/Tag';
+import Tag from '../components/Tag';
 import RecipeCard from '../components/RecipeCard';
+import { Connection, getRepository } from 'typeorm';
+import { DatabaseHandler } from '../database/database';
+import { isError } from '../util/Result';
+import { Recipe as RecipeEntity } from '../database/entities/Recipe';
+import { Tag as TagEntity } from '../database/entities/Tag';
 
 type RecipeOverviewProps = NativeStackScreenProps<
   RootStackParamList,
   'RecipeOverview'
 >;
 
-interface Recipe {
+interface RecipeCard {
   id: number;
   icon: string;
   title: string;
@@ -18,7 +23,7 @@ interface Recipe {
   tags: Tag[];
 }
 
-const TEST_RECIPE_DATA: Recipe[] = [
+const TEST_RECIPE_DATA: RecipeCard[] = [
   {
     id: 1,
     icon: '🍔',
@@ -27,13 +32,13 @@ const TEST_RECIPE_DATA: Recipe[] = [
     tags: [
       {
         id: 1,
-        name: 'aaaaaa'
+        name: 'aaaaaa',
       },
       {
         id: 2,
-        name: 'other'
-      }
-    ]
+        name: 'other',
+      },
+    ],
   },
   {
     id: 2,
@@ -43,26 +48,76 @@ const TEST_RECIPE_DATA: Recipe[] = [
     tags: [
       {
         id: 1,
-        name: 'food'
+        name: 'food',
       },
       {
         id: 2,
-        name: 'other'
-      }
-    ]
-  }
+        name: 'other',
+      },
+    ],
+  },
 ];
 
 const RecipeOverview = ({ navigation }: RecipeOverviewProps) => {
+  const [database, setDatabase] = useState<Connection | undefined>();
+  const [recipeCards, setRecipeCards] = useState<RecipeCard[]>([]);
 
-  const renderItem = ({item}: {item: Recipe}) => (
-    <RecipeCard icon={item.icon} title={item.title} tags={item.tags}/>
+  const setupConnection = async () => {
+    const connection = await DatabaseHandler.getDbConnection();
+    if (!isError(connection)) {
+      setDatabase(connection);
+    } else {
+      console.log(`error: ${connection.message}`);
+    }
+  };
+
+  const getTags = async () => {
+    const tagRepository = getRepository(TagEntity);
+    let result = await tagRepository.find();
+    if (result.length === 0) {
+      const tag1 = new TagEntity();
+      tag1.name = 'food';
+      const tag2 = new TagEntity();
+      tag2.name = 'other';
+      await tagRepository.save(tag1);
+      await tagRepository.save(tag2);
+    }
+  };
+
+  const getRecipes = async () => {
+    const tagRepository = getRepository(TagEntity);
+    let tagResult = await tagRepository.find();
+
+    const recipeRepository = getRepository(RecipeEntity);
+    let recipeResult = await recipeRepository.find();
+    if (recipeResult.length === 0) {
+      const newRecipe = new RecipeEntity();
+      newRecipe.title = 'Example 1';
+      newRecipe.icon = '😀';
+      newRecipe.description = '';
+      newRecipe.tags = tagResult;
+      recipeResult = await recipeRepository.find();
+    }
+    setRecipeCards(recipeResult);
+  };
+
+  useEffect(() => {
+    if (!database) {
+      setupConnection();
+    } else {
+      getTags();
+      getRecipes();
+    }
+  }, []);
+
+  const renderItem = ({ item }: { item: RecipeCard }) => (
+    <RecipeCard icon={item.icon} title={item.title} tags={item.tags} />
   );
 
   return (
     <FlatList
       style={styles.overview}
-      data={TEST_RECIPE_DATA}
+      data={recipeCards}
       renderItem={renderItem}
       keyExtractor={(item) => item.id.toString()}
       overScrollMode='always'
